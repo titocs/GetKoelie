2f60bef73954a1e883f24ea801b516befdd86ae3
Hfn <55591161+titocs@users.noreply.github.com>
2022-05-31 00:20:02 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+        fontFamily: {
+            'poppins' : ['Poppins', 'sans-serif'],
+        },
+        colors: {
+            'coklatCustom' : '#3C373D',
+        },
+    },
+  },
+  plugins: [],
+}
